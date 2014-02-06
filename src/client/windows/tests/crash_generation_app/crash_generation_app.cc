0cd9ff5643923298ebf3f7bafda754f30ddd5017
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-02-06 00:11:12 +0000

    Fix building client library and upload tools for MinGW
    This is the rest of https://breakpad.appspot.com/548002/, brought up to date
    
    Mainly fix include filenames to be lowercased for case-sensitive filesystems
    
    Removed usage of stdext::checked_array_iterator, which is a MSVC-only feature.

--- src/client/windows/tests/crash_generation_app/crash_generation_app.cc
+++ src/client/windows/tests/crash_generation_app/crash_generation_app.cc
@@ -42,6 +42,10 @@
 
 #include "client/windows/tests/crash_generation_app/abstract_class.h"
 
+#ifdef __MINGW32__
+#define swprintf_s swprintf
+#endif
+
 namespace google_breakpad {
 
 const int kMaxLoadString = 100;
@@ -474,9 +478,11 @@ int APIENTRY _tWinMain(HINSTANCE instance,
   CustomClientInfo custom_info = {kCustomInfoEntries, kCustomInfoCount};
 
   CrashServerStart();
+#ifdef _MSC_VER
   // This is needed for CRT to not show dialog for invalid param
   // failures and instead let the code handle it.
   _CrtSetReportMode(_CRT_ASSERT, 0);
+#endif
   handler = new ExceptionHandler(L"C:\\dumps\\",
                                  NULL,
                                  google_breakpad::ShowDumpResults,
