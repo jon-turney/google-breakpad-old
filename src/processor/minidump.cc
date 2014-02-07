2f86d479915940da785cba8afeff1d0f84dd5ce5
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-02-07 23:03:10 +0000

    Default debug_file to the code_file basename
    Strip any path from code_file, and use that as debug_file if one isn't recorded
    in the codeview record.
    
    The current implementation of 'ld --build-id' doesn't write a pdbfilename in the
    codeview record, not least because we don't have such a thing.
    
    The minidump processor requires both debug_file and debug_identifier (the
    build-id) to locate the symbol file.

--- src/processor/minidump.cc
+++ src/processor/minidump.cc
@@ -41,6 +41,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <libgen.h>
 
 #ifdef _WIN32
 #include <io.h>
@@ -2483,6 +2484,16 @@ string MinidumpModule::debug_file() const {
         }
       }
     }
+  }
+
+  // Manufacture debug-file from code-file
+  if (file.empty()) {
+    char *c_filename = strdup(code_file().c_str());
+    char *base = basename(c_filename);
+    file = base;
+    free(c_filename);
+
+    BPLOG(INFO) << "Generated debug_file '" << file << "' from code_file '" << *name_ << "'";
   }
 
   // Relatively common case
