fd6b05473b5211265757603e992b983c0eedd8d4
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-07-12 14:09:03 +0100

    Remove local copy of curl headers
    This reverts commit d9d863e153d12532fc85bd8b44bc47f079ed064c.
    This reverts commit 68e380d5a73a0d657693f166fbcb1dd77027766b.

--- src/common/linux/libcurl_wrapper.h
+++ src/common/linux/libcurl_wrapper.h
@@ -30,11 +30,12 @@
 // A wrapper for libcurl to do HTTP Uploads, to support easy mocking
 // and unit testing of the HTTPUpload class.
 
+#include <curl/curl.h>
+
 #include <string>
 #include <map>
 
 #include "common/using_std_string.h"
-#include "third_party/curl/curl.h"
 
 namespace google_breakpad {
 class LibcurlWrapper {
